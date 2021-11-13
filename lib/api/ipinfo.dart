d9caa615362e4fcc2f6e91609a3f16ab79107f8d
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-11-13 22:02:13 +0600

    direct

--- lib/api/ipinfo.dart
+++ lib/api/ipinfo.dart
@@ -4,22 +4,18 @@ import 'package:ghuri_test_app/models/ip_data_model.dart';
 import 'package:http/http.dart';
 
 Future<IpDataModel?> getData() async {
-  var request = Request(
-      'GET',
-      Uri.parse(
-          'http://api.ipapi.com/114.134.94.220?access_key=8ef48a6483be725fa8471de0fd09884d&format=1'));
+ var request = Request('GET', Uri.parse('http://api.ipapi.com/114.134.94.220?access_key=8ef48a6483be725fa8471de0fd09884d&format=1'));
 
-  StreamedResponse response = await request.send();
 
-  if (response.statusCode == 200) {
-    var body = await response.stream.bytesToString();
-    IpDataModel encodedData = IpDataModel.fromRawJson(body);
-    print(body);
-    return encodedData;
-  } else {
-    print(response.reasonPhrase);
-    return null;
-  }
+StreamedResponse response = await request.send();
+
+if (response.statusCode == 200) {
+  print(await response.stream.bytesToString());
+}
+else {
+  print(response.reasonPhrase);
+}
+
 }
 
 
